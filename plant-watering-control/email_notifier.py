6015a7f421e752b2bcc912d7ef91421e316e1215
benny <benfroelich@gmail.com>
2019-08-10 14:50:08 -0700

    simpler email that is working for now

--- plant-watering-control/email_notifier.py
+++ plant-watering-control/email_notifier.py
@@ -1,114 +1,26 @@
-from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import smtplib, ssl
 from email.mime.text import MIMEText
-import base64
-# missing from docs
-from apiclient import errors
+from email.mime.multipart import MIMEMultipart
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+import credentials # email username and password
+
+def send_notification(recipients, subject, body):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = credentials.username
+    message["To"] = recipients
+    message.attach(MIMEText(body, "plain"))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(credentials.username, credentials.password)
+        server.sendmail(credentials.username, recipients, message.as_string())
 
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    #email_notify("test message", "hi benny", "benfroelich@gmail.com")
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            print("creds need to be refreshed")
-            print(creds.expired)
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '../credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    send_notification("benfroelich@gmail.com", "test message", "message contents here")
 
-    service = build('gmail', 'v1', credentials=creds)
-
-    # send a test message
-    msg = create_message("plantwateringproject1@gmail.com", 
-        recipients, subject, message)
-    send_message(service, "me", msg)
-
-
-def email_notify(subject, message, recipients):
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '../credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('gmail', 'v1', credentials=creds)
-    msg = create_message("plantwateringproject1@gmail.com", 
-        recipients, subject, message)
-    send_message(service, "me", msg)
-
-def create_message(sender, to, subject, message_text):
-  """Create a message for an email.
-
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
-
-  Returns:
-    An object containing a base64url encoded email object.
-  """
-  message = MIMEText(message_text)
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-  raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
-  return {'raw': raw}
-
-def send_message(service, user_id, message):
-  """Send an email message.
-
-  Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    message: Message to be sent.
-
-  Returns:
-    Sent Message.
-  """
-  try:
-    message = (service.users().messages().send(userId=user_id, body=message)
-               .execute())
-    print('Message Id: %s' % message['id'])
-    return message
-  except errors.HttpError as error:
-    print('An error occurred: %s' % error)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
