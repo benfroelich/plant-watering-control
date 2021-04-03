ab2806dce2a6fcf5094eedcb745a124dfc7042ef
benny <benfroelich@gmail.com>
2021-04-02 22:09:23 -0700

    add comment

--- plant-watering-control/logData.py
+++ plant-watering-control/logData.py
@@ -9,6 +9,7 @@ db = MySQLdb.connect(
 curs = db.cursor()
 
 def log_data(data, sensorName, units):
+    """ inject data into the plant watering database """
     curs.execute("""INSERT INTO """ + os.environ["PLANT_WATERING_DB_TABLE"] + \
             """ values(CURRENT_TIMESTAMP, %s, %s, %s)""", (sensorName, data, units))
     db.commit()
